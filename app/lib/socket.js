a7e2615e704aae0a40f262e50a6343f719906d60
oguz-adiguzel <oguz_adiguzel@outlook.com>
2026-07-14 16:40:14 +0300

    socket.io edit deneme 1

--- app/lib/socket.js
+++ app/lib/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-export const socket = io(" https://dostagider-api.vercel.app", {
+export const socket = io("https://dostagider-api.vercel.app", {
   autoConnect: false,
   withCredentials: true,
 });
