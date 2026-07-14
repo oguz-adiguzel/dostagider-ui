4a1befc0f5a329ab665fa82a51d17f7df5be1a05
oguz-adiguzel <oguz_adiguzel@outlook.com>
2026-07-14 17:03:31 +0300

    socket url render.com

--- app/lib/socket.js
+++ app/lib/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-export const socket = io("https://dostagider-api.vercel.app", {
+export const socket = io("https://dostagider-api.onrender.com", {
   autoConnect: false,
   withCredentials: true,
 });
